fbcc9c8da95102e924a19ae8fbec2d6eb05c7809
Sumin <tnalsdldkr2@naver.com>
2024-10-13 21:52:56 +0900

    Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+// CSS Module
+import style from './index.module.css'
+
 export default function Home() {
-  return <h1>Index</h1>
+  return (
+    <>
+      <h1 className={style.h1}>Index</h1>
+      <h2 className={style.h2}>Index</h2>
+    </>
+  )
 }
