c6b34954be9f205e02c68970102668c596a18f3d
Sumin <tnalsdldkr2@naver.com>
2024-10-14 15:53:36 +0900

    SEO 설정

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -42,7 +42,7 @@ export default function Page() {
       <Head>
         <title>한입북스 - 검색 결과</title>
         <meta property='og:image' content='/thumnail.png' />
-        <meta property='og:title' content='한입북스' />
+        <meta property='og:title' content='한입북스 - 검색결과' />
         <meta property='og:description' content='한입 북스에 등록된 도서들을 만나보세요' />
       </Head>
       {books.map((book) => (
